0f3a0a44af485398fb214b0e503525ac643ca404
KaioRapanos <kaiorapanos@gmail.com>
2023-07-08 21:42:39 -0300

    first commit

---
+++ src/main/java/agenda/model/JavaBeans.java
@@ -0,0 +1,73 @@
+package agenda.model;
+
+import java.util.Objects;
+
+public class JavaBeans {
+
+	private String idcon;
+	private String nome;
+	private String fone;
+	private String email;
+
+	public JavaBeans() {
+		super();
+	}
+
+	public JavaBeans(String idcon, String nome, String fone, String email) {
+		super();
+		this.idcon = idcon;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
+
+	public String getIdcon() {
+		return idcon;
+	}
+
+	public void setIdcon(String idcon) {
+		this.idcon = idcon;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idcon);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JavaBeans other = (JavaBeans) obj;
+		return Objects.equals(idcon, other.idcon);
+	}
+
+}
