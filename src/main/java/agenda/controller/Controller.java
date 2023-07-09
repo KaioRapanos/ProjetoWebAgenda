ad7dfb2c902177e785db82922239104f06e20e31
KaioRapanos <kaiorapanos@gmail.com>
2023-07-09 14:07:26 -0300

    Second commit

--- src/main/java/agenda/controller/Controller.java
+++ src/main/java/agenda/controller/Controller.java
@@ -1,39 +1,81 @@
 package agenda.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import agenda.model.DAO;
+import agenda.model.JavaBeans;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import agenda.model.DAO;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/controller", "/main"})
+@WebServlet(urlPatterns = {"/controller", "/main", "/insert"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Controller() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	protected void doGet(HttpServletRequest request, 
+	HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getServletPath();
+		System.out.println(action);
+		if(action.equals("/main")) {
+			contatos(request, response);
+		}else if(action.equals("/insert")) {
+			novoContato(request, response);
+		}else {
+			response.sendRedirect("index.html");
+		}
 		//Teste de conexão
-		dao.testeConexao();
+		//dao.testeConexao();
+	}
+	protected void novoContato(HttpServletRequest request, 
+	HttpServletResponse response) throws ServletException, IOException {
+		//teste de recebimento dos dados do formulario
+		//System.out.println(request.getParameter("nome"));
+		//System.out.println(request.getParameter("fone"));
+		//System.out.println(request.getParameter("email"));
+		//setar as variaveis JavaBeans
+		
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		
+		// invocar metodo inserirContato passando obj contato
+		dao.inserirContato(contato);
+		
+		// redirecionar para o documento agenda.jsp
+		response.sendRedirect("main");
+		
 	}
 
+	//Listar contatos
+	protected void contatos(HttpServletRequest request, 
+	HttpServletResponse response) throws ServletException, IOException {
+		//Criando um obj que ira receber os dados JavaBeans
+		ArrayList<JavaBeans> lista = dao.listarcontatos();
+		
+		// teste recebimento lista
+		//for(int i = 0 ; i<lista.size() ; i++) {
+		//System.out.println(lista.get(i).getIdcon());
+		//System.out.println(lista.get(i).getNome());
+		//System.out.println(lista.get(i).getFone());
+		//System.out.println(lista.get(i).getEmail());
+		//}
+		
+		// Encaminhar a lista ao documento agenda,jsp
+		
+		request.setAttribute("contatos", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+		rd.forward(request, response);
+		
+	}
 }
--- src/main/java/agenda/model/DAO.java
+++ src/main/java/agenda/model/DAO.java
@@ -2,15 +2,18 @@ package agenda.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DAO {
 	/** Módulo de conexão **/
 	// Parâmetros de conexão
 	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url = "jdbc:mysql://127.0.0.1:3306/"
-			+ "dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String url = "jdbc:mysql://127.0.0.1:3306/" + "dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
 	private String password = "1234567";
+
 	// Método de conexão
 	private Connection conectar() {
 		Connection con = null;
@@ -19,19 +22,67 @@ public class DAO {
 			con = DriverManager.getConnection(url, user, password);
 			return con;
 		} catch (Exception e) {
-			System.out.println(e);;
+			System.out.println(e);
+			;
 			return null;
 		}
 	}
-	// Teste de conexão
-	public void testeConexao() {
+
+	// **CRUD CREATE**/
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos (nome,fone,email)" + "values (?,?,?)";
 		try {
+			// abrir conexao com banco de dados
 			Connection con = conectar();
-			System.out.println(con);
+			// preparar a query para a execuco no banco da dados
+			PreparedStatement pst = con.prepareStatement(create);
+			// subistituir os parametros(?)
+			// pelo conteudo das variaveir JavaBeans
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+
+			// executar a query
+			pst.executeUpdate();
+			// encerrar conexao com o banco de dados
 			con.close();
 		} catch (Exception e) {
-			System.out.println(e);;
+			System.out.println(e);
 		}
 	}
-}
 
+	/** CRUDE READ **/
+	public ArrayList<JavaBeans> listarcontatos() {
+		// criando obj para acessar JavaBeans
+		ArrayList<JavaBeans> contatos = new ArrayList<>();
+
+		String read = "select * from contatos order by nome";
+		try {
+			Connection con = conectar();
+			PreparedStatement pst = con.prepareStatement(read);
+			ResultSet rs = pst.executeQuery();
+			// laco abaixo sera executado enquato haver contatos
+			while (rs.next()) {
+				// variaveis de apoio que recebem dados de apoio
+				String idcon = rs.getString(1);
+				String nome = rs.getString(2);
+				String fone = rs.getString(3);
+				String email = rs.getString(4);
+				// armazenar no vetor dinamico (ArrayList)
+				contatos.add(new JavaBeans(idcon, nome, fone, email));
+			}
+			con.close();
+			return contatos;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+
+	}
+
+	/*
+	 * Teste de conexão public void testeConexao() { try { Connection con =
+	 * conectar(); System.out.println(con); con.close(); } catch (Exception e) {
+	 * System.out.println(e);; } }
+	 */
+}
